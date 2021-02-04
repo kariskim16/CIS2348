12647f18665da9e542f94d944e8dde5d72b43522
kariskim16 <77806643+kariskim16@users.noreply.github.com>
2021-02-04 14:24:25 -0600

    Add files via upload

---
+++ Homework 1/1_20.py
@@ -0,0 +1,13 @@
+def userinput():
+    user_num1 = int(input("Enter Integer: "))
+    user_square = user_num1 * user_num1
+    user_cube = user_num1 * user_num1 * user_num1
+
+    print("You entered", user_num1)
+    print(user_num1, "squared is", user_square)
+    print(user_num1, "cubed is", user_cube, "!!")
+
+    user_num2 = int(input("Enter another integer: "))
+    print(user_num1, "+", user_num2, "is", user_num1 + user_num2)
+    print(user_num1, "*", user_num2, "is", user_num1 * user_num2)
+userinput()
