e83462cb661a03d15151b37e3b31f86f0655f4d5
kariskim16 <77806643+kariskim16@users.noreply.github.com>
2021-05-04 23:18:41 -0500

    Add files via upload

---
+++ Homework4/14_11.py
@@ -0,0 +1,14 @@
+def selection_sort_descend_trace(lst):
+    for n in range(len(lst) - 1):
+        largest_value = n
+        for j in range(n + 1, len(lst)):
+            if lst[j] > lst[largest_value]:
+                largest_value = j
+        lst[n], lst[largest_value] = lst[largest_value], lst[n]
+        for i in lst:
+            print(i, end=' ')
+        print()
+    return lst
+if __name__ == '__main__':
+    numbers = [int(i) for i in input().split()]
+    selection_sort_descend_trace(numbers)
